302856af1643ca81ae9954a065b70b9e103f1ff2
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-12 11:53:04 -0600

    Check README

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+};
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,6 +20,6 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
